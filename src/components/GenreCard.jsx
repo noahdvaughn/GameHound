12db72b9e8a0f903da9b3ca07d3933faaae3917d
Noah Vaughn <noahdvaughn@gmail.com>
2023-02-03 11:43:33 -0500

    added GenereCard

--- src/components/GenreCard.jsx
+++ src/components/GenreCard.jsx
@@ -1,12 +1,13 @@
-const GenreCard = () => {
+const GenreCard = ({image, onClick, name, gamesCount, }) => {
 
   return (
-    <div className="card">
-      <div className="img-wrapper">
+    <div className="card" onClick={onClick}>
+      <div className="img-wrapper" src={image} alt="Game Image">
 
       </div>
       <div className="info-wrapper flex-col">
-        
+        <h3>{name}</h3>
+        <p>{gamesCount}</p>
       </div>
     </div>
   )
