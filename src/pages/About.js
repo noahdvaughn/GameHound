d801ba6642fa84236026058572c737544ba3c038
Noah Vaughn <noahdvaughn@gmail.com>
2023-02-04 21:43:43 -0500

    finished dog theming and about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,26 @@
 const About = () => {
   return (
-    <div>
-      <h1>Example about text</h1>
+    <div className="aboutDiv">
+      <h1 className="aboutText">
+        Game Hound is a React-built app that uses the RAWG API to sniff out
+        games that the user wants to fetch and retrieves the game info to the
+        user for a treat. Built on week 4 of the General Assembly Software
+        Immersive program. In other words - "bark bark woof yip yip sniff sniff"
+      </h1>
+      <div className="socialLinks">
+        <a href="https://github.com/noahdvaughn">
+          <img
+            src="http://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/2b4584429ff7662.png"
+            alt=""
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/noahvaughn/">
+          <img
+            src="http://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/0baeb31aedf8e64.png"
+            alt=""
+          />
+        </a>
+      </div>
     </div>
   )
 }
